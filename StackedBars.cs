fcef89294b5b695461b71b84c6768544663c0708
AzumattDev <azumattgaming@gmail.com>
2024-11-04 22:59:36 -0500

    1.0.5

--- StackedBars.cs
+++ StackedBars.cs
@@ -15,7 +15,7 @@ namespace StackedBars
     public class StackedBarsPlugin : BaseUnityPlugin
     {
         internal const string ModName = "StackedBars";
-        internal const string ModVersion = "1.0.4";
+        internal const string ModVersion = "1.0.5";
         internal const string Author = "Azumatt";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
